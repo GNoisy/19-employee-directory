fc91e0120c715a53752905fc9f06d8bc80abaefd
Giovanni Noisy <gnoisy77@gmail.com>
2020-08-13 22:45:51 -0700

    added code for jumbotron and table

---
+++ employeedirectory/src/components/Jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron jumbotron-fluid">
+        <div className="constainer">
+            <h1 className="display-4">Employee Directory</h1>
+            <p className="lead">This directory allows you to search for employees alphabetically or sort by department.</p>
+        </div>
+    </div>
+  );
+}
+
+export default Jumbotron;
---
+++ employeedirectory/src/components/Table/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Table (props) {
+    return (
+        <tbody>
+            <tr>
+            <th scope="row"><img src={props.image}/></th>
+                <td> { props.name } </td>
+                <td> { props.department } </td>
+                <td> { props.phoneNumber } </td>
+            </tr>
+        </tbody>
+    );
+}
+
+export default Table;
