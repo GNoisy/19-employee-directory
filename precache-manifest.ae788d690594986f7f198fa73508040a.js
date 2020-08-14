2e9e2824a88af64f42ba6dba62594c6d850fa46d
Giovanni Noisy <gnoisy77@gmail.com>
2020-08-14 14:43:31 -0700

    Updates

---
+++ precache-manifest.ae788d690594986f7f198fa73508040a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d811c8cf51dd354b74e1a704ce66d2e9",
+    "url": "/19-employee-directory/index.html"
+  },
+  {
+    "revision": "e0acbe85ee13f31e6aca",
+    "url": "/19-employee-directory/static/css/main.8926c30b.chunk.css"
+  },
+  {
+    "revision": "fc512b80fb25f96298eb",
+    "url": "/19-employee-directory/static/js/2.c3fe75fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/19-employee-directory/static/js/2.c3fe75fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0acbe85ee13f31e6aca",
+    "url": "/19-employee-directory/static/js/main.ab417870.chunk.js"
+  },
+  {
+    "revision": "408434c6ea5b2a9c67ee",
+    "url": "/19-employee-directory/static/js/runtime-main.abe65106.js"
+  }
+]);
